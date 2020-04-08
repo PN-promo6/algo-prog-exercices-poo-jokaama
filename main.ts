397c2697763a39dc22d4738854e087bf26606127
Camaridine <mbechezi.camaridine@gmail.com>
2020-04-08 15:51:49 +0200

    Correction exercice 5 en TS

--- main.ts
+++ main.ts
@@ -2,30 +2,31 @@ class ConcertTicket {
   nameTicket:string;
   idPlace: number;
   artist:string;
-  type:string = "";
+  typePlace:string = "";
 
 
-  constructor(nameTicket: string, idPlace: number, artist:string, type?:string) {
-    this.nameTicket = price;
+  constructor(nameTicket: string, idPlace: number, artist:string, typePlace?:string) {
+    this.nameTicket = nameTicket;
     this.idPlace = idPlace;
     this.artist = artist;
-    this.type = type;
+    if (typePlace) {
+      this.typePlace = typePlace;
+    };
   }
   htmlDetails(): string {
-    let content: `
+    let content: string = `
     <p>Votre nom : ${this.nameTicket}</p>
     <p>Numéro de la place : ${this.idPlace}</p>
     <p>Nom de l'artiste : ${this.artist}</p> </br>
     `;
-    if (this.type = "Dans la fosse") {
-      content = content +  `<p>Type de place: Dans la fosse`
-    } else {
-      content + `<p>Type de place: Assise`
+    if (this.typePlace != "") {
+      content = content +  `<p>Type de place: ${this.typePlace}</p>`
     }
+      return (content);
   }
-  return (content);
+
 }
 let ownerTicket01: ConcertTicket = new ConcertTicket("Florian Thauvin",269013, "Sniper", "Dans la fosse");
-let ownerTicket02: ConcertTicket = new ConcertTicket("Florian Thauvin",269013, "Sniper", "Assise");
+let ownerTicket02: ConcertTicket = new ConcertTicket("Florian Thauvin",269013, "Sniper");
 console.log(ownerTicket01.htmlDetails());
 console.log(ownerTicket02.htmlDetails());
